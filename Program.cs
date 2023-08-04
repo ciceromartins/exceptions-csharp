23a0356cbbc57fd47e7cb3ab21e2939025f07157
Cícero Martins <ociceromartins@icloud.com>
2023-08-04 08:22:58 -0300

    Exceptions em C# . NET

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+  var numero1 = Convert.ToInt32(Console.ReadLine());
+
+  Console.WriteLine("EXECUTADO!");
+}
+catch (FormatException ex)
+{
+  Console.WriteLine("Erro de conversão!");
+}
+finally
+{
+  Console.WriteLine("EXECUTO SEMPRE!");
+}
